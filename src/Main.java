201934eb20a38384db86bef5e55be811cded8d26
Max Wank <maxwank11@gmail.com>
2022-10-28 11:59:25 -0400

    added questions

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         clear();
         intro();
         wait(1500);
+        clear();
         question1();
     }
     public static void clear() {
@@ -30,33 +31,181 @@ public class Main {
     public static void question1(){
         System.out.println("What is 1+1?");
         int answer=sc.nextInt();
-
+        if (answer == 2){
+            wait(100);
+            clear();
+            System.out.print("Correct!");
+            wait(500);
+            clear();
+        } else {
+            wait(100);
+            clear();
+            System.out.println("Incorrect...");
+            wait(500);
+            clear();
+            question1();
+        }
     }
     public static void question2(){
-
+        System.out.println("What is 1+1?");
+        int answer=sc.nextInt();
+        if (answer == 2){
+            wait(100);
+            clear();
+            System.out.print("Correct!");
+            wait(500);
+            clear();
+        } else {
+            wait(100);
+            clear();
+            System.out.println("Incorrect...");
+            wait(500);
+            clear();
+            question1();
+        }
     }
     public static void question3(){
-
+        System.out.println("What is 1+1?");
+        int answer=sc.nextInt();
+        if (answer == 2){
+            wait(100);
+            clear();
+            System.out.print("Correct!");
+            wait(500);
+            clear();
+        } else {
+            wait(100);
+            clear();
+            System.out.println("Incorrect...");
+            wait(500);
+            clear();
+            question1();
+        }
     }
     public static void question4(){
-
+        System.out.println("What is 1+1?");
+        int answer=sc.nextInt();
+        if (answer == 2){
+            wait(100);
+            clear();
+            System.out.print("Correct!");
+            wait(500);
+            clear();
+        } else {
+            wait(100);
+            clear();
+            System.out.println("Incorrect...");
+            wait(500);
+            clear();
+            question1();
+        }
     }
     public static void question5(){
-
+        System.out.println("What is 1+1?");
+        int answer=sc.nextInt();
+        if (answer == 2){
+            wait(100);
+            clear();
+            System.out.print("Correct!");
+            wait(500);
+            clear();
+        } else {
+            wait(100);
+            clear();
+            System.out.println("Incorrect...");
+            wait(500);
+            clear();
+            question1();
+        }
     }
     public static void question6(){
-
+        System.out.println("What is 1+1?");
+        int answer=sc.nextInt();
+        if (answer == 2){
+            wait(100);
+            clear();
+            System.out.print("Correct!");
+            wait(500);
+            clear();
+        } else {
+            wait(100);
+            clear();
+            System.out.println("Incorrect...");
+            wait(500);
+            clear();
+            question1();
+        }
     }
     public static void question7(){
-
+        System.out.println("What is 1+1?");
+        int answer=sc.nextInt();
+        if (answer == 2){
+            wait(100);
+            clear();
+            System.out.print("Correct!");
+            wait(500);
+            clear();
+        } else {
+            wait(100);
+            clear();
+            System.out.println("Incorrect...");
+            wait(500);
+            clear();
+            question1();
+        }
     }
     public static void question8(){
-
+        System.out.println("What is 1+1?");
+        int answer=sc.nextInt();
+        if (answer == 2){
+            wait(100);
+            clear();
+            System.out.print("Correct!");
+            wait(500);
+            clear();
+        } else {
+            wait(100);
+            clear();
+            System.out.println("Incorrect...");
+            wait(500);
+            clear();
+            question1();
+        }
     }
     public static void question9(){
-
+        System.out.println("What is 1+1?");
+        int answer=sc.nextInt();
+        if (answer == 2){
+            wait(100);
+            clear();
+            System.out.print("Correct!");
+            wait(500);
+            clear();
+        } else {
+            wait(100);
+            clear();
+            System.out.println("Incorrect...");
+            wait(500);
+            clear();
+            question1();
+        }
     }
     public static void question10(){
-
+        System.out.println("What is 1+1?");
+        int answer=sc.nextInt();
+        if (answer == 2){
+            wait(100);
+            clear();
+            System.out.print("Correct!");
+            wait(500);
+            clear();
+        } else {
+            wait(100);
+            clear();
+            System.out.println("Incorrect...");
+            wait(500);
+            clear();
+            question1();
+        }
     }
 }
